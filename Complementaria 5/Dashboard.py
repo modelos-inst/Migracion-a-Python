9f633aa8e2c84526aae9df0d1409e343f4f45003
Juan Pablo Rios <125321594+jpablo700@users.noreply.github.com>
2024-12-19 20:44:52 -0500

    Add files Comp5: Intro Dashboard (#7)
    * Add files via upload
    
    Traducción de Complementaria 5: Introducción a Dashboards. 
    Archivo app_ejemplo es un dashboard de ejemplo de la clase Analítica Computacional para que los estudiantes corran y así tengan una primera aproximación a los Dash.
    Como los dashboards no pueden ejecutarse directamente desde un Jupyter Notebook, el archivo Complementaria5_IntroShiny.ipynb contiene la explicación de la complementaria (lo que antes era el HTML en Bloque Neón), el archivo Funciones.py contiene lo que antes conocíamos como el Script auxiliar donde ejecutábamos las cadenas de markov y en Dashboard.py se ejecuta la herramienta, llamando a las funciones del script auxiliar (Funciones.py).
    
    * ajustes a comple5
    
    ---------
    
    Co-authored-by: juanfperez <juanfernando.perez@gmail.com>

---
+++ Complementaria 5/Dashboard.py
@@ -0,0 +1,84 @@
+# Librerías para ejecutar el dash
+import dash
+from dash import dcc  # dash core components
+from dash import html # dash html components
+from dash.dependencies import Input, Output
+
+# Librería para graficar
+import plotly.graph_objs as go
+
+# Este comando importa todas las funciones del archivo Funciones.py
+# El archivo Funciones.py se debe encontrar en la misma carpeta que este documento.
+from Funciones import *
+
+# Enlace a la hoja de estilos CSS y permite construir la herramiente Dash
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+# Se crea el objeto app, dentro del cual se extrae el atributo server
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+
+# Definir la interfaz de la aplicación
+app.layout = html.Div([
+    # Titulo de la aplicación
+    html.H1('Aplicación Políticas de Inventario', style={'textAlign': 'center'}),
+
+    # Se realiza la división de recuadros entre el título y los demás objetos
+    html.Div([
+    # Imprimir gráfica de costos
+    dcc.Graph(id='Graf_Cost_Tot'),
+
+    # Crear objeto para ingresar parametro de tasa de demanda con un control deslizante
+    dcc.Slider(
+            id = 'TasaDemanda',
+            min = 0,
+            max = 30,
+            value = 15,
+            step = 1
+        ),
+    
+    # Crear objeto para especificar la tasa seleccionada por el usuario y las unidades
+    html.Div(id='valorSlider')
+    ])
+])
+
+@app.callback(
+    # Outputs que se van a modificar cuando el usuario interactua
+    [Output('Graf_Cost_Tot', 'figure'),
+     Output('valorSlider', 'children')],
+     # Input en el que se va a fijar el callback
+    [Input('TasaDemanda', 'value')])
+def update_graph(Tasa):
+    # Importamos la información de la funcion en Funciones.py
+    InfoCostos = dash_function(Tasa)
+
+    #__________Generar gráfica de los costos para las próximas 10 semanas__________
+    # cada variable trace guardada es una nueva curva a la gráfica
+    trace1 = go.Scatter(
+    x=InfoCostos['numero_semana'],
+    y=InfoCostos['costo_semanal_PolActual'],
+    mode='lines+markers',
+    name='P Actual'
+    )
+
+    trace2 = go.Scatter(
+    x=InfoCostos['numero_semana'],
+    y=InfoCostos['costo_semanal_PolNueva'],
+    mode='lines+markers',
+    name='P nueva'
+    )
+
+    layout = go.Layout(
+    title='Costo por política en las próximas semanas',
+    xaxis=dict(title='Semana'),
+    yaxis=dict(title='Costo')
+    )
+
+    fig = go.Figure(data=[trace1, trace2], layout=layout)
+
+    return fig, f'Tasa seleccionada: {Tasa} cajas/semana'
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ Complementaria 5/Funciones.py
@@ -0,0 +1,99 @@
+# Script donde se llevaran a cabo todas las funciones
+
+# Librerías que vamos a utilizar
+# Recuerde emplear pip install en una terminal si no ha instalado alguna de ellas
+import pandas as pd
+import numpy as np
+
+from scipy.stats import poisson
+from jmarkov.dtmc import dtmc
+
+# Crear la función que realice los procedimientos necesarios.
+# En este caso se busca crear los objetos cadenas de markov para ambas políticas, 
+# realizar el análisis transitorio para las 10 semanas y obtener los respectivos costos
+def dash_function(tasa):
+    
+    # Crear los estados
+    estados = range(23)
+
+    # Crear y llenar la matriz P de la política actual 
+    matrizP = np.zeros((len(estados), len(estados)), dtype = float)
+
+    # Para la Política Actual -> si i<=10 solicita 12 resmas
+    for i in estados:
+        for j in estados:
+            if i<=10 and j>0:
+                matrizP[i,j] = poisson.pmf(12+i-j, tasa) #poisson.pmf calcula P(D=k)
+            elif i<=10 and j==0:
+                matrizP[i,j] = poisson.sf(12+i-1, tasa) #poisson.smf calcula P(D>k). Por esto es necesario restar 1 unidad.
+            elif i>10 and j>0:
+                matrizP[i,j] = poisson.pmf(i-j, tasa)
+            elif i>10 and j==0:
+                matrizP[i,j] = poisson.sf(i-1, tasa)
+
+    
+    # Crear y llenar la matriz P para el caso de la Política Nueva
+    matrizPNueva = np.zeros((len(estados), len(estados)), dtype = float)
+    
+    # Para la nueva Política  -> solicitar hasta la capacidad máxima
+    for i in estados:
+        for j in estados:
+            if j>0:
+               matrizPNueva[i,j] = poisson.pmf(22-j, tasa)
+            elif j==0:
+                matrizPNueva[i,j]=poisson.sf(21, tasa)
+
+    # Crear las dos cadenas usando el paquete jmarkov
+    # Política Actual
+    politica_Actual = dtmc(matrizP)
+    # Política Nueva
+    politica_Nueva = dtmc(matrizPNueva)
+
+    # Valor de los costos de inventario y de ordenar
+    cInventario = 6200
+    cOrdenar = 38000
+
+    # Estimar los costos para las próximas 10 semanas
+    # Definir vector de estado inicial dado que al final de esta semana quedaron cero unidades en inventario
+    alfa = np.zeros(len(estados))
+    alfa[0] = 1
+
+    # Calcula el vector de costos de la política actual 
+    cost_Sem_Pactual = []
+    # Calcula el costo de inventario y el costo de ordenar promedio de la política actual
+    cost_inv_Pactual = []
+    cost_ord_Pactual = []
+
+    for i in range(1,11):
+        probs = politica_Actual.transient_probabilities(n = i, alpha=alfa)
+
+        print(cInventario * np.dot(probs, estados))
+        cost_inv_Pactual.append(cInventario * np.dot(probs, estados))
+        cost_ord_Pactual.append(cOrdenar*sum(probs[0:10]))
+
+    print(cost_inv_Pactual)
+    print(cost_ord_Pactual)
+    cost_Sem_Pactual = np.add(cost_inv_Pactual, cost_ord_Pactual)
+
+    # Calcula el vector de costos de la política nueva
+    cost_Sem_Pnueva = []
+    # Calcula el costo de inventario y el costo de ordenar promedio de la política nueva
+    cost_inv_Pnueva = []
+    cost_ord_Pnueva = [] 
+    for i in range(1,11):
+        probs = politica_Nueva.transient_probabilities(n = i, alpha=alfa)
+        
+        cost_inv_Pnueva.append(cInventario * np.dot(probs, estados))
+        cost_ord_Pnueva.append(cOrdenar*sum(probs[0:21]))
+    
+    cost_Sem_Pnueva = np.add(cost_inv_Pnueva, cost_ord_Pnueva)
+
+    # Vector de numero de semana
+    num_sem = range(1,11)
+    data = pd.DataFrame({
+        'numero_semana': num_sem,
+        'costo_semanal_PolActual': cost_Sem_Pactual,
+        'costo_semanal_PolNueva': cost_Sem_Pnueva
+    })
+
+    return data
